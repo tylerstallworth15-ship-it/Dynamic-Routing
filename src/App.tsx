07c5b896b871bb0bc0f5905f2483c299a846c3c6
Tyler Stallworth <ps.tstallworth@outlook.com>
2026-01-13 10:51:02 -0500

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BlogIndex from "./pages/BlogIndex";
@@ -34,34 +33,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import React from 'react';
-import { Routes, Route useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-
-import Navbar from './components/Navbar';
-import ProtectedRoute from './components/ProtectedRoute';
-import PageWrapper from './components/Pagewrapper';
-
-import Home from './pages/Home';
-import BlogIndex from './pages/BlogIndex';
-import BlogPost from './pages/BlogPost';
-import Admin from './pages/Admin';
-import Login from './pages/Login';
-
-const App: React.FC = () => {
-  const location = use Location();
-
-  return (
-    <>
-      <Navbar />
-      <main className='main-content'>
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-          </Routes>
-        </AnimatePresence>
-      </main>
-  )
-}
->>>>>>> a0d72d8dc6a3a28c5fa01bb3ca1785c90b82d258
